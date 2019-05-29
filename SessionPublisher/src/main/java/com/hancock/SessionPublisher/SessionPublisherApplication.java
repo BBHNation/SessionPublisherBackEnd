444f9b14569f6158e08c455b48b0be226f90cd19
Hancock <binhan.bai@thoughtworks.com>
2019-05-29 15:13:40 +0800

    [INIT] - init project

---
+++ SessionPublisher/src/main/java/com/hancock/SessionPublisher/SessionPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.hancock.SessionPublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionPublisherApplication.class, args);
+	}
+
+}
---
+++ SessionPublisher/src/test/java/com/hancock/SessionPublisher/SessionPublisherApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hancock.SessionPublisher;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SessionPublisherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

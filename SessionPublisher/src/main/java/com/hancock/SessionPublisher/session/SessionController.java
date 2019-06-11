eae64d754a870dc8a2c742540639d7e4d43639a2
Hancock <bai.szsy@qq.com>
2019-06-11 11:29:33 +0800

    [TOKEN] - add checker in session controller

--- SessionPublisher/src/main/java/com/hancock/SessionPublisher/session/SessionController.java
+++ SessionPublisher/src/main/java/com/hancock/SessionPublisher/session/SessionController.java
@@ -1,6 +1,7 @@
 package com.hancock.SessionPublisher.session;
 
 import com.hancock.SessionPublisher.intrastructure.exceptions.NotFoundException;
+import com.hancock.SessionPublisher.intrastructure.utils.UserTokenChecker;
 import com.hancock.SessionPublisher.session.views.CreateSessionRequest;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,35 +12,42 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/sessions")
 public class SessionController {
     private final SessionApplicationService applicationService;
+    private final UserTokenChecker userTokenChecker;
 
     @Autowired
-    public SessionController(SessionApplicationService applicationService) {
+    public SessionController(SessionApplicationService applicationService,
+        UserTokenChecker userTokenChecker) {
         this.applicationService = applicationService;
+        this.userTokenChecker = userTokenChecker;
     }
 
     @GetMapping(value = "/list")
-    public List<SessionDomain> sessionDomainList() {
+    public List<SessionDomain> sessionDomainList(@RequestHeader("token") String token, @RequestHeader("email") String email) {
+        userTokenChecker.checkUserTokenValid(email, token);
         return applicationService.getSessionDomainList();
     }
 
     @GetMapping(value = "/detail/{sessionId}")
-    public SessionDomain sessionInfo(@PathVariable String sessionId) {
+    public SessionDomain sessionInfo(@PathVariable String sessionId, @RequestHeader("token") String token, @RequestHeader("email") String email) {
+        userTokenChecker.checkUserTokenValid(email, token);
         return applicationService.findSessionById(sessionId);
     }
 
     @PutMapping(value = "")
-    public void createSession(@RequestBody @Validated CreateSessionRequest request)
-        throws NotFoundException {
+    public void createSession(@RequestBody @Validated CreateSessionRequest request, @RequestHeader("token") String token, @RequestHeader("email") String email) {
+        userTokenChecker.checkUserTokenValid(email, token);
         applicationService.createSession(request);
     }
 
     @PostMapping(value = "/{sessionId}/nextStage")
-    public void gotoNextStage(@PathVariable String sessionId) {
+    public void gotoNextStage(@PathVariable String sessionId, @RequestHeader("token") String token, @RequestHeader("email") String email) {
+        userTokenChecker.checkUserTokenValid(email, token);
         applicationService.sessionGotoNextStage(sessionId);
     }
 
     @PostMapping(value = "/{sessionId}/publish")
-    public void publishSession(@PathVariable String sessionId) {
+    public void publishSession(@PathVariable String sessionId, @RequestHeader("token") String token, @RequestHeader("email") String email) {
+        userTokenChecker.checkUserTokenValid(email, token);
         applicationService.publishSession(sessionId);
     }
 

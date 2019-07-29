3d744bd87dd2fbbbd370bd0e9575df7ccb69c22e
Hancock <bai.szsy@qq.com>
2019-07-29 14:02:25 +0800

    [TECH] - delete some no use file.

--- SessionPublisher/src/main/java/code/war/FindOdd.java
+++
@@ -1,9 +0,0 @@
-package code.war;
-
-import java.util.Arrays;
-
-public class FindOdd {
-    public static int findIt(int[] a) {
-        return Arrays.stream(a).reduce((init, next)-> init ^ next).orElse(0);
-    }
-}
